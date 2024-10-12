2a6436ba3c6e6c281cc2babe26085655bd94bf20
Sheldon Codling <codlinsh@gmail.com>
2024-10-12 03:18:26 -0400

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html", "*/**/**/.js"],
+  theme: {
+    extend: {
+      colors: {
+        "wargroove-brown": "#ad855a",
+        "wargroove-white": "#f4f0e9",
+      }
+    },
+  },
+  plugins: [],
+}
+
