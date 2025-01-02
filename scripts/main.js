2e55129ec00ef8df0e0e6f562a10809e88d41281
sheldon <shelcod@gmail.com>
2025-01-02 02:25:16 -0500

    finished merging function

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,49 @@
 import Unit, { units } from "./unit.js"
 "use strict";
 
-
+let formState = {} // Global object to keep track of the state of the form
 
 /**
- * Brief description of what the function does.
+ * Function to show the menus. Basically just removes a tailwind class
  *
- * @param {bool} isAttacking - bool to determin whether to show attacking or defending unit menu
- * @returns {null} Description of the return value.
- * @example
- * // Example usage of the function
- * menu.addEventListener("click", () => {showMenu(true)})
+ * @param {bool} isUnit - Bool to determin if we are selecting unit menu or terrain menu
+ * @param {bool} isAttacking - Bool to determin whether to show menu for attacking or defending player
+ * @returns {null} Does not return anything
+ * @example document.getElementById("attacking_unit").addEventListener("click", () => {showMenu(true, true)});
  */
-function showMenu(isAttacking) {
-    let menu = isAttacking ? document.getElementById("attack_menu") : document.getElementById("defend_menu");
+function showMenu(isUnit, isAttacking) {
+    let menu = null;
+    
+    if (isUnit) {
+        menu = isAttacking ? document.getElementById("attack_menu") : document.getElementById("defend_menu");
+    }
+    else {
+        menu = isAttacking ? document.getElementById("attack_terrain_menu") : document.getElementById("defend_terrain_menu");
+    }
 
     // Remove translate class
     menu.classList.remove("translate-y-offscreen");
 }
 
 function updateForm(isUnit, isAttacking) {
-    let menu
-    const currSelections;
-    const currSelectionImg;
+    let menu = null;
+    let currSelections = null;
+    let currSelectionImg = null;
+    let selection = null;
     
     if (isUnit) {
-        let menu = isAttacking ? document.getElementById("attack_menu") : document.getElementById("defend_menu");
-        const currSelections = isAttacking ? document.getElementsByName("attacker") : document.getElementsByName("defender");
-        const currSelectionImg = isAttacking ? document.getElementById("attacking_unit") : document.getElementById("defending_unit");
+        menu = isAttacking ? document.getElementById("attack_menu") : document.getElementById("defend_menu");
+        currSelections = isAttacking ? document.getElementsByName("attacker") : document.getElementsByName("defender");
+        currSelectionImg = isAttacking ? document.getElementById("attacking_unit") : document.getElementById("defending_unit");
     } 
     else {
-        let menu = isAttacking ? document.getElementById("attack_terrain_menu") : document.getElementById("defend_terrain_menu");
-        const currSelections = isAttacking ? document.getElementsByName("attack_terrain") : document.getElementsByName("defend_terrain")
-        const currSelectionImg = isAttacking ? document.getElementById("attacking_env") : document.getElementById("defending_env");
+        menu = isAttacking ? document.getElementById("attack_terrain_menu") : document.getElementById("defend_terrain_menu");
+        currSelections = isAttacking ? document.getElementsByName("attack_terrain") : document.getElementsByName("defend_terrain")
+        currSelectionImg = isAttacking ? document.getElementById("attacking_env") : document.getElementById("defending_env");
     }
-    
-    let selection = null;
+
+    console.log("isUnit: " + isUnit);
+    console.log("isAttacking: " + isAttacking);
 
     // Check to see what unit is selected
     for (const currSelect of currSelections) {
@@ -48,10 +56,24 @@ function updateForm(isUnit, isAttacking) {
     if (isAttacking === false) {
         selection = selection.replace("_d", "");
     }
-    // Update the GUI
-    currSelectionImg.src = isAttacking ? "/assets/images/sprites/Cherrystone/" + selectedUnit + ".png" :  "/assets/images/sprites/Felheim/" + selectedUnit + ".png";
+    // Update the GUI and update state
+    if (isUnit) {
+        currSelectionImg.src = isAttacking ? "/assets/images/sprites/Cherrystone/" + selection + ".png" :  "/assets/images/sprites/Felheim/" + selection + ".png";
+        if (isAttacking) formState.attackUnit = selection 
+        else formState.defendUnit = selection;
+    }
+    else {
+        currSelectionImg.src = "/assets/images/terrain/" + selection + ".png";
+        if (isAttacking) formState.attackTerrain = selection.replace("/(sheild|danger)\d*/g", (match) => {
+            console.log(match);
+        })
+        else formState.defendTerrain = Number(selection.replace("/sheild|danger/g", (match) => {
+            if (match === "sheild") return "";
+            if (match === "danger") return "-";
+        })); 
+    }
 
-    console.log("Selected Unit: ", selection);
+    console.log(formState);
 
     // Readd class after 250ms
     setTimeout(() => {
@@ -61,13 +83,6 @@ function updateForm(isUnit, isAttacking) {
     }, 100);
 }
 
-function showTerrain(isAttacking) {
-    let menu = isAttacking ? document.getElementById("attack_terrain_menu") : document.getElementById("defend_terrain_menu");
-    
-    // Remove translate class
-    menu.classList.remove("translate-y-offscreen");
-}
-
 // Event handlers
 // UNITS
 document.getElementById("attacking_unit").addEventListener("click", () => {showMenu(true, true)});
@@ -75,7 +90,7 @@ document.getElementById("attack_menu_form").addEventListener("change", () => {up
 document.getElementById("defending_unit").addEventListener("click", () => {showMenu(true, false)});
 document.getElementById("defend_menu_form").addEventListener("change", () => {updateForm(true, false)});
 //TERRAINS
-document.getElementById("attacking_env").addEventListener("click", () => {showTerrain(false, true)});
-document.getElementById("attack_terrain_menu").addEventListener("change", () => {updateTerrain(false, true)})
-document.getElementById("defending_env").addEventListener("click", () => {showTerrain(false, false)});
-document.getElementById("defend_terrain_menu").addEventListener("change", () => {updateTerrain(false, false)})
+document.getElementById("attacking_env").addEventListener("click", () => {showMenu(false, true)});
+document.getElementById("attack_terrain_menu").addEventListener("change", () => {updateForm(false, true)})
+document.getElementById("defending_env").addEventListener("click", () => {showMenu(false, false)});
+document.getElementById("defend_terrain_menu").addEventListener("change", () => {updateForm(false, false)})
