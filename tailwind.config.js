6d71a929302845249d1475ee77d3a7299345c8f0
Sheldon Codling <codlinsh@gmail.com>
2025-01-08 00:48:28 -0500

    Added more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         "wargroove-brown": "#ad855a",
         "wargroove-white": "#f4f0e9",
+        "wargroove-black": "#3d4265",
+        "wargroove-red": "#aa003f",
+        "wargroove-blue": "#335eb0",
       },
       translate: {
         "offscreen": "101vh",
